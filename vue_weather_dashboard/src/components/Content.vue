f807a4e00347b312a33ba622c0eb273aaf0babf6
Merve Erdogmus <merve.erdogmus@adcolony.com>
2020-08-04 11:51:38 +0300

    Added root component and content component to define structure

---
+++ vue_weather_dashboard/src/components/Content.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>This child components of Content.vue are:</p>
+    <ul>
+      <li v-for="child in childComponents">{{ child }}</li>
+    </ul>
+     {{ weather_data }}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["weather_data"],// props array to receive prop from parent
+  data () {
+    return {
+      childComponents: ['TempVarChart.vue', 'Highlights.vue']
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
