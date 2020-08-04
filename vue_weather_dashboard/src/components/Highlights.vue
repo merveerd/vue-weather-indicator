9aa35d3373bab16a2629920af66c335ca5d7ee41
Merve Erdogmus <merve.erdogmus@adcolony.com>
2020-08-04 12:55:53 +0300

    Dummy version of all child components

--- vue_weather_dashboard/src/components/Highlights.vue
+++ vue_weather_dashboard/src/components/Highlights.vue
@@ -2,7 +2,7 @@
   <div id="highlights">
     <p>Weather Highlights:</p>
     {{ highlights }}
-    <uv-index :highlights="highlights"></uv-index>
+    <uv-index :highlights="highlights"></uv-index>  <!-- in other way; <uv-index :uvindex="uvindex"></uv-index> --> 
     <visibility :highlights="highlights"></visibility>
     <wind-status :highlights="highlights"></wind-status>  
   </div>
@@ -14,15 +14,15 @@ import Visibility from './Visibility.vue';
 import WindStatus from './WindStatus.vue';
 
 export default {
-  props: ["highlights"],
-  components: {
+  props: ["highlights"], //as the highlights once is defined in parent(1. level), no need to define again in the return of data for 3. level components(grandchildren)
+  components: { //we defined what are gonna be rendered:
     'uv-index': UVIndex,
     'visibility': Visibility,
     'wind-status': WindStatus,
   },  
   data () {
     return {
-
+// in other way; uvindex : this.highlights.uvindex
     }
   },
   methods: {
---
+++ vue_weather_dashboard/src/components/UVIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="uvindex">
+    <p>UV Index: {{ uvindex }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ["highlights"], // in other way;  props: ["uvindex"],
+  data () {
+    return {
+      uvindex: this.highlights.uvindex // in other way; remove this
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
